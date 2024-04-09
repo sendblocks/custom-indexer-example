a0fe02a461c299412da6a27224e1d53a419a2481
Michael Kellner <michael@sendblocks.io>
2024-04-09 18:38:37 +0300

    re-ordered files to fit cli directory structure

---
+++ functions/sb-bayc.js
@@ -0,0 +1,64 @@
+import { ethers } from "https://cdn.skypack.dev/ethers@v5.7.2";
+
+const eventIface = new ethers.utils.Interface([
+    "event Approval(address indexed owner, address indexed approved, uint256 indexed tokenId)",
+    "event ApprovalForAll(address indexed owner, address indexed operator, bool approved)",
+    "event OwnershipTransferred(address indexed previousOwner, address indexed newOwner)",
+    "event Transfer(address indexed from, address indexed to, uint256 indexed tokenId)",
+]);
+
+
+export async function triggerHandler(context, data) {
+    const parsedLog = eventIface.parseLog(data);
+
+    switch (parsedLog.name) {
+        case "Transfer":
+            return await handleTransfer(parsedLog);
+        case "Approval":
+            return await handleApproval(parsedLog);
+    }
+}
+
+async function handleTransfer(parsedLog) {
+    const { from, to, tokenId } = parsedLog.args;
+    const hexTokenId = tokenId.toHexString();
+    const tokenKey = `token-${hexTokenId}`;
+
+    // Fetch the token object fron KV store
+    let token = await sbcore.storage.Load("BAYC-storage", tokenKey, null);
+    if (token === null) {
+        token = {
+            owner: to,
+            previousOwner: from,
+            approved: null,
+            tokenId: hexTokenId
+        };
+    } else {
+        token.previousOwner = from;
+        token.owner = to;
+        token.approved = null;
+    }
+
+    await sbcore.storage.Store("BAYC-storage", tokenKey, token);
+}
+
+async function handleApproval(parsedLog) {
+    const { owner, approved, tokenId } = parsedLog.args;
+    const hexTokenId = tokenId.toHexString();
+    const tokenKey = `token-${hexTokenId}`;
+
+    // Fetch the token object fron KV store
+    let token = await sbcore.storage.Load(tokenKey, null);
+    if (token === null) {
+        token = {
+            owner: owner,
+            previousOwner: null,
+            approved: approved,
+            tokenId: hexTokenId
+        };
+    } else {
+        token.approved = approved;
+    }
+
+    await sbcore.storage.Store(tokenKey, token);
+}
